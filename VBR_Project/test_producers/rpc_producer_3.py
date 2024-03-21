7320ed755520ea57f852e5a6ddae367ae12a00c1
MontaGGGne <shchukinaa2002@gmail.com>
2024-03-21 16:09:53 +0300

    Shchukin: attempt by several producers

--- VBR_Project/test_producers/rpc_producer_3.py
+++ VBR_Project/test_producers/rpc_producer_3.py
@@ -1,4 +1,13 @@
-import requests, json, struct, pika, sys, os
+import sys, os, pika, json, logger
+import pandas as pd
+import time
+import boto3
+import dagshub
+import json
+import os
+from dagshub import streaming
+
+PROD_NUM = 3
 
 HOST = 'localhost'
 PORT = 7801
@@ -13,8 +22,8 @@ EXCHANGE='dataset-reader'
 EXCHANGE_TYPE='topic'
 QUEUE_REQUEST='dataset-reader-request'
 QUEUE_RESPONSE='dataset-reader-response'
-ROUTING_KEY_REQUEST='request.*'
-ROUTING_KEY_RESPONSE='response.*'
+ROUTING_KEY_REQUEST=f'request.{PROD_NUM}'
+ROUTING_KEY_RESPONSE=f'response.{PROD_NUM}'
 
 
 def pika_connection():
@@ -31,8 +40,8 @@ def pika_connection():
 def main():
     connection = pika_connection()
     
-    channel = connection.channel()
-
+    channel = connection.channel()  
+    
     channel.exchange_declare(exchange=EXCHANGE, exchange_type=EXCHANGE_TYPE, durable=True)
 
     channel.queue_declare(queue=QUEUE_REQUEST, durable=True)
@@ -40,28 +49,43 @@ def main():
 
     channel.queue_declare(queue=QUEUE_RESPONSE, durable=True)
     channel.queue_bind(exchange=EXCHANGE, queue=QUEUE_RESPONSE, routing_key=ROUTING_KEY_RESPONSE)
-
+    
+    
     def callback(ch, method, properties, body):
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-        
         print(body)
 
-        ch.basic_publish(exchange=EXCHANGE, 
-                        routing_key=ROUTING_KEY_RESPONSE, 
-                        body=body)
+    channel.basic_consume(queue=QUEUE_RESPONSE, on_message_callback=callback)
 
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue=QUEUE_REQUEST, on_message_callback=callback)
-
-    print('[Server_RabbitMQ] Waiting for messages')
-    channel.start_consuming()
-
+    fs = streaming.DagsHubFilesystem(".", repo_url=REPO_URL, token=TOKEN)
+    csv_file_str = fs.http_get(os.path.join(URL_PATH_STORAGE, 'test_FD001.csv'))
+    
+    csv_as_list_of_dicts = []
+    columns_names = []  
+    list_csv = []
+    list_csv = csv_file_str.text.split('\n')
+    columns_names = list_csv[0].split(',')
+    list_csv.pop(0)
+    for data_line in list_csv:
+        if data_line is None or data_line == '':
+            continue
+        dict_from_line_in_csv = {}
+        data_list = data_line.split(',')
+        for col_id, col_name in enumerate(columns_names):
+            dict_from_line_in_csv[col_name] = data_list[col_id]
+            
+            channel.basic_publish(exchange=EXCHANGE,
+                                  routing_key=ROUTING_KEY_REQUEST,
+                                  body=json.dumps(dict_from_line_in_csv))
+            
+        csv_as_list_of_dicts.append(dict_from_line_in_csv)
+    
+    connection.process_data_events(time_limit=None)
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print("[Server_RabbitMQ] Interrupted")
+        print("Request Interrupted")
         try:
             sys.exit(0)
         except SystemExit:

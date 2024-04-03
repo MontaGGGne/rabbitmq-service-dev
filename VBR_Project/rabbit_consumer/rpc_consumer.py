eccdabcaf787bb3c0f504187e3e1f1a4224124b8
MontaGGGne <shchukinaa2002@gmail.com>
2024-04-03 21:14:22 +0300

    Shchukin: preparing containers

--- VBR_Project/rabbit_consumer/rpc_consumer.py
+++ VBR_Project/rabbit_consumer/rpc_consumer.py
@@ -1,4 +1,6 @@
-import requests, json, struct, pika, sys, os
+import sys
+import os
+from rmq_custom_pack import rpc_consumer
 
 HOST = 'localhost'
 PORT = 7801
@@ -13,52 +15,19 @@ ROUTING_KEY_REQUEST='request.*'
 ROUTING_KEY_RESPONSE='response.*'
 
 
-def pika_connection():
-    connection = pika.BlockingConnection(
-                pika.ConnectionParameters(
-                    host=HOST,
-                    port=PORT,
-                    credentials=pika.PlainCredentials(
-                        USER,
-                        PASSWORD)))
-    return connection
-
-
 def main():
-    connection = pika_connection()
-    
-    channel = connection.channel()
-
-    channel.exchange_declare(exchange=EXCHANGE, exchange_type=EXCHANGE_TYPE, durable=True)
-
-    channel.queue_declare(queue=QUEUE_REQUEST, durable=True)
-    channel.queue_bind(exchange=EXCHANGE, queue=QUEUE_REQUEST, routing_key=ROUTING_KEY_REQUEST)
-
-    channel.queue_declare(queue=QUEUE_RESPONSE, durable=True)
-    channel.queue_bind(exchange=EXCHANGE, queue=QUEUE_RESPONSE, routing_key=ROUTING_KEY_RESPONSE)
-
-    def callback(ch, method, properties, body):
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-        
-        print(body)
-
-        ch.basic_publish(exchange=EXCHANGE, 
-                        routing_key=ROUTING_KEY_RESPONSE, 
-                        body=body)
-
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue=QUEUE_REQUEST, on_message_callback=callback)
-
-    print('[Server_RabbitMQ] Waiting for messages')
-    channel.start_consuming()
+    consumer = rpc_consumer.Consumer(host=HOST,
+                            port=PORT,
+                            user=USER,
+                            password=PASSWORD,
+                            exchange=EXCHANGE,
+                            exchange_type=EXCHANGE_TYPE,
+                            queue_request=QUEUE_REQUEST,
+                            queue_response=QUEUE_RESPONSE,
+                            r_key_request=ROUTING_KEY_REQUEST,
+                            r_key_response=ROUTING_KEY_RESPONSE)
+    consumer.consumer_handler()
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("[Server_RabbitMQ] Interrupted")
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+    main()
--- VBR_Project/rabbit_producer/rpc_producer.py
+++ VBR_Project/rabbit_producer/rpc_producer.py
@@ -1,94 +1,39 @@
-import sys, os, pika, json, logger
-import pandas as pd
-import time
-import boto3
-import dagshub
-import json
+import sys
 import os
-from dagshub import streaming
+from rmq_custom_pack import rpc_producer
 
-PROD_NUM = 1
+PROD_NUM=os.getenv('PROD_NUM')
 
-HOST = 'localhost'
-PORT = 7801
-USER = 'rmuser'
-PASSWORD = 'rmpassword'
+HOST=os.getenv('HOST')
+PORT=os.getenv('PORT')
+USER=os.getenv('USER')
+PASSWORD=os.getenv('PASSWORD')
 
-REPO_URL = 'https://dagshub.com/Dimitriy200/diplom_autoencoder'
-TOKEN = 'a1482d904ec14cd6e61aa6fcc9df96278dc7c911'
-# https://dagshub.com/Dimitriy200/diplom_autoencoder/src/main/data/raw
-URL_PATH_STORAGE = 'https://dagshub.com/api/v1/repos/Dimitriy200/diplom_autoencoder/raw/ee325159c4cd9c796be0ea038c9272b8dc10626d/data/raw/'
+REPO_URL=os.getenv('REPO_URL')
+TOKEN=os.getenv('TOKEN')
+URL_PATH_STORAGE=os.getenv('URL_PATH_STORAGE')
 
-EXCHANGE='dataset-reader'
-EXCHANGE_TYPE='topic'
-QUEUE_REQUEST='dataset-reader-request'
-QUEUE_RESPONSE='dataset-reader-response'
-ROUTING_KEY_REQUEST=f'request.{PROD_NUM}'
-ROUTING_KEY_RESPONSE=f'response.{PROD_NUM}'
-
-
-def pika_connection():
-    connection = pika.BlockingConnection(
-                pika.ConnectionParameters(
-                    host=HOST,
-                    port=PORT,
-                    credentials=pika.PlainCredentials(
-                        USER,
-                        PASSWORD)))
-    return connection
+EXCHANGE=os.getenv('EXCHANGE')
+EXCHANGE_TYPE=os.getenv('EXCHANGE_TYPE')
+QUEUE_REQUEST=os.getenv('QUEUE_REQUEST')
+QUEUE_RESPONSE=os.getenv('QUEUE_RESPONSE')
+ROUTING_KEY_REQUEST=f'{os.getenv('ROUTING_KEY_REQUEST')}{PROD_NUM}'
+ROUTING_KEY_RESPONSE=f'{os.getenv('ROUTING_KEY_RESPONSE')}{PROD_NUM}'
 
 
 def main():
-    connection = pika_connection()
-    
-    channel = connection.channel()  
-    
-    channel.exchange_declare(exchange=EXCHANGE, exchange_type=EXCHANGE_TYPE, durable=True)
+    producer = rpc_producer.Producer(host=HOST,
+                            port=PORT,
+                            user=USER,
+                            password=PASSWORD,
+                            exchange=EXCHANGE,
+                            exchange_type=EXCHANGE_TYPE,
+                            queue_request=QUEUE_REQUEST,
+                            queue_response=QUEUE_RESPONSE,
+                            r_key_request=ROUTING_KEY_REQUEST,
+                            r_key_response=ROUTING_KEY_RESPONSE)
+    producer.producer_handler(repo_url=REPO_URL, token=TOKEN, url_path_storage=URL_PATH_STORAGE)
 
-    channel.queue_declare(queue=QUEUE_REQUEST, durable=True)
-    channel.queue_bind(exchange=EXCHANGE, queue=QUEUE_REQUEST, routing_key=ROUTING_KEY_REQUEST)
-
-    channel.queue_declare(queue=QUEUE_RESPONSE, durable=True)
-    channel.queue_bind(exchange=EXCHANGE, queue=QUEUE_RESPONSE, routing_key=ROUTING_KEY_RESPONSE)
-    
-    
-    def callback(ch, method, properties, body):
-        print(body)
-
-    channel.basic_consume(queue=QUEUE_RESPONSE, on_message_callback=callback)
-
-    fs = streaming.DagsHubFilesystem(".", repo_url=REPO_URL, token=TOKEN)
-    # https://dagshub.com/api/v1/repos/Dimitriy200/diplom_autoencoder/raw/ee325159c4cd9c796be0ea038c9272b8dc10626d/data/raw/test_FD001.csv
-    csv_file_str = fs.http_get(os.path.join(URL_PATH_STORAGE, 'test_FD001.csv'))
-    
-    csv_as_list_of_dicts = []
-    columns_names = []  
-    list_csv = []
-    list_csv = csv_file_str.text.split('\n')
-    columns_names = list_csv[0].split(',')
-    list_csv.pop(0)
-    for data_line in list_csv:
-        if data_line is None or data_line == '':
-            continue
-        dict_from_line_in_csv = {}
-        data_list = data_line.split(',')
-        for col_id, col_name in enumerate(columns_names):
-            dict_from_line_in_csv[col_name] = data_list[col_id]
-            
-            channel.basic_publish(exchange=EXCHANGE,
-                                  routing_key=ROUTING_KEY_REQUEST,
-                                  body=json.dumps(dict_from_line_in_csv))
-            
-        csv_as_list_of_dicts.append(dict_from_line_in_csv)
-    
-    connection.process_data_events(time_limit=None)
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("Request Interrupted")
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+    main()

9b87befbe62c1f19a9793297f08b87aaad0941ef
MontaGGGne <shchukinaa2002@gmail.com>
2024-05-02 22:27:29 +0300

    Shchukin: new ideas for Dockerfile-s

--- VBR_Project/rabbit_consumer/rpc_consumer.py
+++ VBR_Project/rabbit_consumer/rpc_consumer.py
@@ -4,23 +4,71 @@ import logging
 import traceback
 from rmq_custom_pack import rpc_consumer
 
+print("""Текст песни «Хороший день»]
+
+[Интро]
+Какой хороший день, какой хороший день (Ты чувствуешь, сынок?)
+Какой хороший день, какой хороший день
+Ebanat on the beat
+
+[Припев]
+Эй, какой хороший день, чтоб пойти на СВО
+Какой хороший день, чтоб разбомбить всех хохлов
+Какой хороший день: нацепил Z на авто
+Просто съезди на Бахмут, просто съезди на Бахмут
+Эй, какой хороший день, чтоб пойти на СВО
+Какой хороший день, чтоб разбомбить всех хохлов
+Какой хороший день: нацепил Z на авто
+Просто съезди на Бахмут, просто съезди на Бахмут
+
+[Куплет]
+Фактор, Бахмут, калаши, выстрелы по Киеву
+У вас нету ядерки, а также просто выбора
+Наше ПВО ебашит, наши воины сильные
+Я знаю, что за стены нами будут летом разбомблены
+Правда победа в кармане сейчас у нас
+Зачем на пары? Пойду я воевать
+Вам всем пизда, вагнера уже у вас
+У-у-у (Ebanat on the beat)
+[Припев]
+Эй, какой хороший день, чтоб пойти на СВО
+Какой хороший день, чтоб разбомбить всех хохлов
+Какой хороший день: нацепил Z на авто
+Просто съезди на Бахмут, просто съезди на Бахмут
+Эй, какой хороший день, чтоб пойти на СВО
+Какой хороший день, чтоб разбомбить всех хохлов
+Какой хороший день: нацепил Z на авто
+Просто съезди на Бахмут, просто съезди на Бахмут""")
+
 logging.basicConfig(level=logging.INFO, filename="py_log_consumer.log",filemode="w",
                     format="%(asctime)s %(levelname)s %(message)s")
 
-HOST=os.getenv('HOST')
-PORT=int(os.getenv('PORT'))
-USER=os.getenv('USER')
-PASSWORD=os.getenv('PASSWORD')
+# HOST=os.getenv('HOST')
+# PORT=int(os.getenv('PORT'))
+# USER=os.getenv('USER')
+# PASSWORD=os.getenv('PASSWORD')
+
+HOST=os.environ.get('HOST')
+PORT=int(os.environ.get('PORT'))
+USER=os.environ.get('USER')
+PASSWORD=os.environ.get('PASSWORD')
 
 print(f"[HOST]: {HOST}, [PORT]: {PORT}, [USER]: {USER}, [PASSWORD]: {PASSWORD}")
 logging.info(f"[HOST]: {HOST}, [PORT]: {PORT}, [USER]: {USER}, [PASSWORD]: {PASSWORD}")
 
-EXCHANGE=os.getenv('EXCHANGE')
-EXCHANGE_TYPE=os.getenv('EXCHANGE_TYPE')
-QUEUE_REQUEST=os.getenv('QUEUE_REQUEST')
-QUEUE_RESPONSE=os.getenv('QUEUE_RESPONSE')
-ROUTING_KEY_REQUEST=os.getenv('ROUTING_KEY_REQUEST')
-ROUTING_KEY_RESPONSE=os.getenv('ROUTING_KEY_RESPONSE')
+# EXCHANGE=os.getenv('EXCHANGE')
+# EXCHANGE_TYPE=os.getenv('EXCHANGE_TYPE')
+# QUEUE_REQUEST=os.getenv('QUEUE_REQUEST')
+# QUEUE_RESPONSE=os.getenv('QUEUE_RESPONSE')
+# ROUTING_KEY_REQUEST=os.getenv('ROUTING_KEY_REQUEST')
+# ROUTING_KEY_RESPONSE=os.getenv('ROUTING_KEY_RESPONSE')
+
+EXCHANGE=os.environ.get('EXCHANGE')
+EXCHANGE_TYPE=os.environ.get('EXCHANGE_TYPE')
+QUEUE_REQUEST=os.environ.get('QUEUE_REQUEST')
+QUEUE_RESPONSE=os.environ.get('QUEUE_RESPONSE')
+ROUTING_KEY_REQUEST=os.environ.get('ROUTING_KEY_REQUEST')
+ROUTING_KEY_RESPONSE=os.environ.get('ROUTING_KEY_RESPONSE')
 
 print(f"""[EXCHANGE]: {EXCHANGE}, 
              [EXCHANGE_TYPE]: {EXCHANGE_TYPE}, 

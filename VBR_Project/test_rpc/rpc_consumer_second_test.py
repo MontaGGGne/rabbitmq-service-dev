e6b9423cb2bccdcc577b22183f86b276db5a7283
MontaGGGne <shchukinaa2002@gmail.com>
2024-05-25 23:52:08 +0300

    Shchukin: make test time dirs

--- VBR_Project/test_rpc/rpc_consumer_second_test.py
+++ VBR_Project/test_rpc/rpc_consumer_second_test.py
@@ -1,4 +1,4 @@
-import requests, json, struct, pika, sys, os
+import requests, json, struct, pika, sys, os, time
 from pathlib import Path
 
 
@@ -54,14 +54,41 @@ def main():
         unit_number = body_dict["unit number"]
         time_in_cycles = body_dict["time in cycles"]
 
+        current_time = time.time()
+        local_time = time.localtime(current_time)
+        cur_day = local_time.tm_mday
+        cur_mounth = local_time.tm_mon
+        cur_year = local_time.tm_year
+        cur_hours = local_time.tm_hour
+        cur_minuts = local_time.tm_min
+        cur_seconds = local_time.tm_sec
+
+        formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", local_time)
+
         current_dir = os.path.join(SCRIPT_DIR, f"unit_number_{unit_number}")
+        time_dir = os.path.join(current_dir, f"{cur_day}-{cur_mounth}-{cur_year}_{cur_hours}-{cur_minuts}")
         if Path(current_dir).exists() is False:
             os.mkdir(current_dir)
-            with open(os.path.join(current_dir, f"time_in_cycles_{time_in_cycles}.json"), 'w') as f:
+            os.mkdir(time_dir)
+            tik_time = time.time()
+            tik_local_time = time.localtime(tik_time)
+            tik_ormatted_time = time.strftime("%Y-%m-%d %H:%M:%S", tik_local_time)
+            print(tik_ormatted_time)
+            print(time_dir)
+            with open(os.path.join(time_dir, f"time_in_cycles_{time_in_cycles}.json"), 'w') as f:
                 json.dump(body_dict, f)
         else:
-            with open(os.path.join(current_dir, f"time_in_cycles_{time_in_cycles}.json"), 'w') as f:
-                json.dump(body_dict, f)
+            for dir in os.listdir(current_dir):
+                dir_name_as_list = dir.split('_')
+                dir_date = dir_name_as_list[0].split('-')
+                dir_time = dir_name_as_list[1].split('-')
+                if cur_day==int(dir_date[0]) and cur_mounth==int(dir_date[1]) and cur_year==int(dir_date[2]) and cur_hours==int(dir_time[0]) and abs(cur_minuts-int(dir_time[1]))<5:
+                    with open(os.path.join(current_dir, dir, f"time_in_cycles_{time_in_cycles}.json"), 'w') as f:
+                        json.dump(body_dict, f)
+                else:
+                    os.mkdir(time_dir)
+                    with open(os.path.join(time_dir, f"time_in_cycles_{time_in_cycles}.json"), 'w') as f:
+                        json.dump(body_dict, f)
 
         # try:
         #     with open(f"unit_number_{unit_number}.json", 'r') as j:
